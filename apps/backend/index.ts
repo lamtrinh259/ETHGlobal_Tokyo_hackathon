00b2a88bdb3fe7b5da700d8503021e58f9beb6cf
Wilson Lau <hello@wilsonplau.com>
2023-04-15 21:17:24 +0900

    test: retry gpt

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -41,23 +41,34 @@ app.post("/api/query", async (req: express.Request, res: express.Response) => {
   try {
     const question = req.body.query;
     const gptRes = await agent.call({
-      input: "Only return the GraphQL query to my following question, do not include additional text: " + question,
+      input:
+        "Only return the GraphQL query to my following question, do not include additional text: " +
+        question,
     });
-    console.log('Human query is', question)
+    console.log("Human query is", question);
     console.log({ gptRes, steps: gptRes.intermediateSteps });
     const graphQLQuery = gptRes.output.replace("\n", "");
     if (graphQLQuery.includes("I don't know."))
       return res.status(400).json({ e: "Try another query." });
     console.log(graphQLQuery);
     const graphqlRes = await gqlRequest(
-      gptRes.intermediateSteps[0].action.tool,
+      gptRes.intermediateSteps[gptRes.intermediateSteps.length - 1].action.tool,
       graphQLQuery
     );
-    console.log('Result is', graphqlRes)
+    console.log("Result is", graphqlRes);
     return res.status(200).json({ result: graphqlRes });
   } catch (e: any) {
-    console.log(e);
-    return res.status(500).json({ e: e.message });
+    const gptRes2 = await agent.call({
+      input: `This is the error I got previously from using the GraphQL query produceed by you: ${e.message},
+    regenerate a new GraphQL query to my following question, keeping in mind the error message I have sent, and do not include additional text: ${req.body.query} `,
+    });
+    const graphQLQuery2 = gptRes2.output.replace("\n", "");
+    const graphqlRes2 = await gqlRequest(
+      gptRes2.intermediateSteps[gptRes2.intermediateSteps.length - 1].action
+        .tool,
+      graphQLQuery2
+    );
+    return res.status(200).json({ result: graphqlRes2 });
   }
 });
 
